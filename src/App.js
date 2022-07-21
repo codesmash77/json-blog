0984bda50d3a97d571846795a6a3b569b8dc5df6
codesmash77 <gokulgtg@gmail.com>
2022-07-22 01:16:47 +0530

    trying

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
       <Header/>
       <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/#/addBlog" element={<AddBlog/>} />
-        <Route path="/#/editBlog/:id" element={<AddBlog/>} />
-        <Route path="/#/Blog/:id" element={<Blog/>} />
-        <Route path="/#/about" element={<Suspense fallback="...Loading"> <About/> </Suspense>} />
-        <Route path="/#/contact" element={<Suspense fallback="...Loading"><Contact/></Suspense>} />
-        <Route path="/#/*" element={<Suspense fallback="...Loading"><NotFound/></Suspense>} />
+        <Route path="/**" element={<Home/>} />
+        <Route path="/**/addBlog" element={<AddBlog/>} />
+        <Route path="/**/editBlog/:id" element={<AddBlog/>} />
+        <Route path="/**/Blog/:id" element={<Blog/>} />
+        <Route path="/**/about" element={<Suspense fallback="...Loading"> <About/> </Suspense>} />
+        <Route path="/**/contact" element={<Suspense fallback="...Loading"><Contact/></Suspense>} />
+        <Route path="/**/*" element={<Suspense fallback="...Loading"><NotFound/></Suspense>} />
       </Routes>
     </div>
   );
